b6bc9239c3ca8f02d76659805c744d77257dcbb5
weliton <welingtonzsilva@gmail.com>
2022-03-23 03:41:40 -0300

    desenvolvendo o front-end

--- view/header.php
+++ view/header.php
@@ -4,9 +4,33 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="assets/css/reset.css"/>
     <link rel="stylesheet" type="text/css" href="assets/css/style.css"/>
     <title>Lek's</title>
 </head>
 <body>
     
 <div class="container">
+<header>
+       <nav >
+            
+            <ul>
+                <li>
+                    <h1><a href="/home">Lek's</a> </h1>
+                </li>
+                
+                <li>
+                    <form class="form-nav" action="" method="get">
+                        <input type="text" id="input-nav" name="search" placeholder="buscar">
+                    </form>
+                </li>
+                
+                <li><a href="/home">Home</a> </li>
+                <li><a href="#">Mensagens</a> </li>
+                <li><a href="#">Novo</a></li>
+                <li><a href="#">Momentos</a></li>
+                <li><a href="#">Curtidas</a></li>
+                <li><a href="#">Config</a></li>
+            </ul>
+       </nav>
+    </header>
--- view/site/home.php
+++ view/site/home.php
@@ -1,7 +1,25 @@
 <?php require __DIR__.'/../header.php';?>
- 
-<h1>
-    Lek's
-</h1>
+    
+    <main>
+         <section class="main">
+            
+             
+             <div class="perfil">
+                
+                    <img src="assets/img/well.jpg" width="50" height="50" alt="">
+                
+
+                    <p > 
+                     <a href="#">Welitinho da massa</a> <br>
+                    Autor de varias peripécias
+                </p>
+             </div>
+           
+           
+            <img src="assets/img/demon.jpg" alt="">
+        </section>
+    </main>
+
+   
 
 <?php require __DIR__.'/../footer.php';?>

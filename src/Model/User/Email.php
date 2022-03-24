a0acb71d58b7a24a8ee0d1b1b10db81991c0dbd6
weliton <welingtonzsilva@gmail.com>
2022-03-24 03:10:20 -0300

    implementada regras de negçio para Email]

---
+++ src/Model/User/Email.php
@@ -0,0 +1,25 @@
+<?php
+    namespace Weliton\Leks\Model\User;
+
+class Email
+{
+    private string $email;
+
+    public function __construct(string $email)
+    {
+        $this->email = $email;
+        $this->validateEmail($email);
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    private function validateEmail(string $email):bool
+    {
+        return filter_var($email, FILTER_VALIDATE_EMAIL);
+    }
+}
+
+

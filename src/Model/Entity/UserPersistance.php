ee3f412510a9db8422536575fe409f6c2fe5e46f
weliton <welingtonzsilva@gmail.com>
2022-03-24 03:28:15 -0300

    implementado classe de persistancia para usuarios

---
+++ src/Model/Entity/UserPersistance.php
@@ -0,0 +1,13 @@
+<?php
+    namespace Weliton\Leks\Model\Entity;
+
+use Weliton\Leks\Model\User\User;
+
+class UserPersistance extends User
+{
+
+    public function __construct()
+    {
+        
+    }
+}
